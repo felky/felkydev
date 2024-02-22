75e749a95d388744380f459a90300618bdb71007
felkan <felixg.nti@gmail.com>
2024-02-23 00:10:49 +0100

    gone!

--- OLD/components/card.js
+++
@@ -1,9 +0,0 @@
-export default function Gallery({ image, title, text, link }) {
-    return (
-        <div className="card">
-            <img src={`/${image}.png`} alt="Card Image" className="cardImg"/>
-            <h2><a href={link}> {title}</a></h2>
-            <p> {text} </p>
-        </div>
-    )
-}
--- OLD/components/footer.js
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-    const currentYear = new Date().getFullYear();
-
-    return (
-            <div style={{ position: "absolute", bottom: 0, fontSize: 10, textAlign: "center"}}>
-                <p>© felix gramell - { currentYear }</p>
-            </div>
-        )
-}
